fbe7343397149103e5e0af065482877076b37e71
Jamshid Ganiev <jamesganiev227@gmail.com>
2023-02-15 23:28:44 +0900

    Solved validparentheses algo problem

--- algorithms/longestCommonPrefix.py
+++ algorithms/longestCommonPrefix.py
@@ -31,22 +31,44 @@ def longestCommonPrefix(strs: list[str]) -> str:
 # Solution2: || I tried to opotimize it by myself, 
                 # First i found the element with least number of chars in the list, divided it into prefixes.
 
-def longestCommonPrefix(strs: list[str]) -> str:
-    result = ""
-    prefix = {}
-    fel = min(strs, key=len)
-    try:
-        for index in range(len(fel)):
-            prefix[index] = fel[:index+1]
+class Solution:
+    def longestCommonPrefix(strs: list[str]) -> str:
+        result = ''
+        prefix = {}
+        shortest_word = min(strs, key=len)
+        for index in range(len(shortest_word)):
+            prefix[index] = shortest_word[:index+1]
             for x in strs:
                 if prefix[index] != x[:index+1]:
                     return result
             result = prefix[index]
         return result
-    except IndexError:
-        return result
 
-# Runtime: 35ms, |beats 73.06% :)
-# M:13.8: |Beats 81.80 :)
+# Runtime: 31ms, |beats 89.51% :)
+# M:13.9: |Beats 41.2% :)
 
-# try to optmize it more   
+# I'll try to optmize it more 
+
+# Soluton3 || Binary Search | R: beats 51%: | M: 41%
+# 
+class Solution:
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+        if strs is None or len(strs) == 0:
+            return ""
+        return self._longest_common_prefix(strs, 0, len(strs) - 1)
+
+    def _longest_common_prefix(self, strs, l, r):
+        if l == r:
+            return strs[l]
+        else:
+            mid = (l + r) // 2
+            lcp_left = self._longest_common_prefix(strs, l, mid)
+            lcp_right = self._longest_common_prefix(strs, mid + 1, r)
+            return self._common_prefix(lcp_left, lcp_right)
+
+    def _common_prefix(self, left, right):
+        min_len = min(len(left), len(right))
+        for i in range(min_len):
+            if left[i] != right[i]:
+                return left[:i]
+        return left[:min_len]  
