1ab6f69fe9106cf77af310183e5bf7c92a81b2c7
Jamshid-Ganiev <jamesganiev227@gmail.com>
2023-05-04 03:19:15 +0900

    contains duplicate

---
+++ leetcode_patterns/contains_duplicate.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/contains-duplicate/
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False
